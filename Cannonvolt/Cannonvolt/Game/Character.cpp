57d11ae1a5ac10523b2ca39c6f2fb206cbf41396
Yoocodehash <57808935+Yoocodehash@users.noreply.github.com>
2020-11-21 16:02:11 -0500

    New Code

--- Cannonvolt/Cannonvolt/Game/Character.cpp
+++ Cannonvolt/Cannonvolt/Game/Character.cpp
@@ -37,3 +37,8 @@ void Character::CollisionResponse(GameObject* obj)
 
 	GameObject::CollisionResponse(obj);
 }
+
+void OnCollisionEnter(Collision collision) {
+	Destroy(collision.collider.gameObject);
+	Destroy(gameObject);
+}
